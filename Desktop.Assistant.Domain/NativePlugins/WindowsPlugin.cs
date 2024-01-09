ee4f281b4e8ff1caa0cd3f28bd1a7b1379627e1a
zyxucp <286513187@qq.com>
2024-01-09 13:32:17 +0800

    fix 修改包依赖关系

---
+++ Desktop.Assistant.Domain/NativePlugins/WindowsPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Assistant.Domain.NativePlugins
+{
+    public class WindowsPlugin
+    {
+        private readonly Kernel _kernel;
+        public WindowsPlugin(Kernel kernel)
+        {
+            _kernel = kernel;
+        }
+    }
+}
